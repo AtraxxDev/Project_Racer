4776edd2660ed15c38ca5800457aa1ef5f28b0c8
AtraxxDev <cdmx2218@amerike.edu.mx>
2023-08-24 00:25:50 -0600

    Script Car

--- Assets/Script/CarController.cs
+++ Assets/Script/CarController.cs
@@ -6,18 +6,36 @@ public class CarController : MonoBehaviour
 {
     public Rigidbody sphererb;
     public float forwardAccel = 8f,reverseAccel = 4f,maxSpeed = 50f, turnStrenght = 100;
+    private float speedInput, turnInput;
     void Start()
     {
-        
+        sphererb.transform.parent = null;
     }
 
     void Update()
     {
+        speedInput = 0f;
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * forwardAccel * 1000f;
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * reverseAccel * 1000f;
+        }
+
+        turnInput = Input.GetAxis("Horizontal");
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrenght * Time.deltaTime, 0));
+
         transform.position = sphererb.transform.position;
     }
 
     private void FixedUpdate()
     {
-        sphererb.AddForce(transform.forward * forwardAccel);
+        if(Mathf.Abs(speedInput) > 0) 
+        {
+            sphererb.AddForce(transform.forward * speedInput);
+        }
     }
 }
